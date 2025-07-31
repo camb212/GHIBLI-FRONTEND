5d6171f437f99554d0b0bbf83091241a9df9547d
Slowlyxs <alejandroalarcon851@gmail.com>
2025-07-31 12:24:36 -0500

    correccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  root: process.cwd(),
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html')
-    },
-    outDir: 'dist'
-  }
 });
